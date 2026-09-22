55ea7cd1a230f8e11f30e3055399e63b13afcfaf
ceciliasandim <cecilia_sandim@estudante.sesisenai.org.br>
2026-09-21 21:09:19 -0300

    Adiciona novo desafio

---
+++ app1.js
@@ -0,0 +1,13 @@
+console.log("================================");
+console.log("         MEU APLICATIVO         ");
+console.log("================================");
+console.log("Nome: Lucas");
+console.log("Cidade: Guaramirim");
+console.log("Atividade de interesse: jogos");
+console.log("Quero aprender: desenvolvimento de aplicativos");
+console.log("Área de interesse: tecnologia");
+console.log("Mensagem: Estou evoluindo na programação!");
+
+console.log("Meu próximo desafio: criar meu primeiro jogo.");
+
+console.log("banana")
